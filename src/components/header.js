fab0f84f625c642da609aa6d662b22ecd851859b
Sithabo <sithabisoh@muwci.net>
2023-08-24 22:11:34 +0530

    added my name and fixed nav padding

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,9 @@ import React from "react";
 function Header(){
     return (
         <div className="header-container">
-            <nav></nav>
+            <nav style={{padding: '1vw'}}>
+                <label style={{fontSize:35, fontFamily: 'Roboto Slab'}}>Sithabo.js</label>
+            </nav>
             <header>
                 <h1>Adventure Awaits</h1>
             </header>
