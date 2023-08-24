3686c5f64cae70d4dd0e2b5f06d4ebf4786d8e5c
Sithabo <sithabisoh@muwci.net>
2023-08-24 14:26:12 +0530

    updated routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import React from 'react';
-import { useEffect, useState } from 'react'; 
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Root from '../components/root';
 import Data from './data';
 
   const route = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={ <Root />}>
-      <Route index element={ <Data />} />
+      <Route path='/Astro' element={ <Data />} />
     </Route>
   ))
 function App() {
